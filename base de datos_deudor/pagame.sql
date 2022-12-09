a5702f058165c6a5d6d464429782167b9cdfc01a
KarenVega <ti19110012@salvatierra.tecnm.mx>
2022-12-09 14:02:36 -0600

    bd_deudorT

---
+++ base de datos_deudor/pagame.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Deudor (
+  idDeudor INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  nombre VARCHAR(45)  NULL  ,
+  direccion VARCHAR(45)  NULL  ,
+  saldo FLOAT  NULL    ,
+PRIMARY KEY(idDeudor));
+
+
+
+
