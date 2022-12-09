02c8dd41d62dbae54c0e91466b61b7b9efef8ced
El Roman <romanromancm@gmail.com>
2022-12-09 14:35:00 -0600

    usuario_java

---
+++ src/pagame/domain/Usuario.java
@@ -0,0 +1,26 @@
+package pagame.domain;
+
+public class Usuario {
+    private String nombre;
+    private String clave;
+    private String tipo;
+
+    public Usuario(String nombre, String clave, String tipo) {
+        this.nombre = nombre;
+        this.clave = clave;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    
+}
