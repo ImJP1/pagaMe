efe5c7e83a0e0285fdaa0c32c1a33278f903df5b
El Roman <romanromancm@gmail.com>
2022-12-09 14:45:56 -0600

    bd_Usuario

---
+++ bd_Usuario/Usuario.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Usuario (
+  idUsuario INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  nombre VARCHAR(45)  NULL  ,
+  clave VARCHAR(255)  NULL  ,
+  tipo VARCHAR(20)  NULL    ,
+PRIMARY KEY(idUsuario));
+
+
+
+
